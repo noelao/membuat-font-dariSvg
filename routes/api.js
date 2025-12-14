9e1864f63ba0c367b2c61cc98ef38aea303cc64b
unknown <bangunjamduamalam@gmail.com>
2025-12-15 01:02:18 +0700

    update bug

--- routes/api.js
+++ routes/api.js
@@ -72,7 +72,11 @@ api.post("/tambahkan-path/:id", authSession, async (req, res) => {
 
     try {
         const data = await ambilJudulId(id);
-        calonData.id = parseInt(data.kumpulan[data.kumpulan.length-1].id) + 1;
+        const lastItem = data.kumpulan[data.kumpulan.length - 1];
+    
+        const lastId = parseInt(lastItem.id, 10) || 0;
+
+        calonData.id = lastId + 1;
     
         data.kumpulan.push(calonData);
         const gabunganData = data.kumpulan;
